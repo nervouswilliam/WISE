3bbec400279a7438cf33e1dad53e87a699dfedb8
nervouswilliam <65856849+nervouswilliam@users.noreply.github.com>
2025-09-10 13:21:08 +0700

    Delete frontend-web/src directory

--- frontend-web/src/models/User.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    sessionId: string;
-    token: string | null;
-    username: string;
-    role: string;
-    image: string;
-    createdTime: number;
-    expiredTime: number;
-}
